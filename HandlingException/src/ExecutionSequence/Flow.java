4fc12ca4d95454309a7bf10f09752d06a8c3e679
Juan Sebastian <juan_seb07@hotmail.com>
2022-10-10 00:01:45 -0500

    Se agrega el packageExecution Sequence en donde se muestra el flujo de ejecucion en Java.

---
+++ HandlingException/src/ExecutionSequence/Flow.java
@@ -0,0 +1,25 @@
+package ExecutionSequence;
+
+public class Flow {
+
+  public static void main(String[] args) {
+    System.out.println("Inicio del main");
+    metodoUno();
+    System.out.println("Fin del main");
+  }
+
+  private static void metodoUno() {
+    System.out.println("Inicio del metodoUno");
+    metodoDos();
+    System.out.println("Fin del metodoUno");
+  }
+
+  private static void metodoDos() {
+    System.out.println("Inicio del metodoDos");
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(i);
+    }
+    System.out.println("Fin del metodoDos");
+  }
+
+}
