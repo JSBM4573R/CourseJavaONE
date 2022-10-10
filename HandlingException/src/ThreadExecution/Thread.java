4c1ebd8158d336c51840f3a635ebbd8377f30ab8
Juan Sebastian <juan_seb07@hotmail.com>
2022-10-10 00:43:20 -0500

    Se agrega el package Thread Execution en donde se mostraran el Stack Trace y el control de Exepciones.

---
+++ HandlingException/src/ThreadExecution/Thread.java
@@ -0,0 +1,27 @@
+package ThreadExecution;
+
+public class Thread {
+
+  public static void main(String[] args) {
+    System.out.println("Inicio Main");
+    methodOne();
+    System.out.println("Fin Main");
+  }
+
+  /**
+   * Este metodo contiene un error en el Stack de ejecucion para poder visualizar
+   * La estructura del error. Esa estructura contiene el nombre, una descripcion y
+   * el Stack Trace, que es la pila de ejecucion del codigo, mostrando en que linea
+   * se rompio el codigo.
+   * En este caso como no se maneja la exepcion el codigo no continua su ejecucion.
+   */
+  private static void methodOne() {
+    System.out.println("Inicio metodo Uno");
+    for (int i = 0; i < 3; i++) {
+      System.out.println(i);
+      int result = i / 0; //Division intensional entre 0 para provocar un Error
+      System.out.println(result);
+    }
+    System.out.println("Fin metodo Uno");
+  }
+}
