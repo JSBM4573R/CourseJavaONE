e498654361b9e964907ca2d1f3c806ebf448aa03
Juan Sebastian <juan_seb07@hotmail.com>
2022-10-11 23:51:49 -0500

    Se agrega la clase ControlException

---
+++ HandlingException/src/ThreadExecution/ControlException.java
@@ -0,0 +1,31 @@
+package ThreadExecution;
+
+public class ControlException {
+
+  public static void main(String[] args) {
+    System.out.println("Inicio Main");
+    methodOne();
+    System.out.println("Fin Main");
+  }
+
+  /**
+   * Este metodo parecido al creado en la clase Thread contiene un error provocado
+   * de forma intencional, pero se agrega un bloque try catch lo que permite atrapar
+   * la excepcion y evita que el codigo se rompa en la pila de ejecucion o Stack.
+   * Se debe crear una exception acorde a la posibilidad de ocurriencia de un error.
+   * Ademas se debe mostrar el Stack Trace para exponer la linea de error.
+   */
+  private static void methodOne() {
+    System.out.println("Inicio metodo Uno");
+    for (int i = 0; i < 3; i++) {
+      System.out.println(i);
+      try {
+        int result = i / 0; //Division intensional entre 0 para provocar un Error
+        System.out.println(result);
+      } catch (ArithmeticException e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.println("Fin metodo Uno");
+  }
+}
