3b6afeb80416d5c6be0cb2e484710a87276dbc75
Juan Sebastian <juan_seb07@hotmail.com>
2022-10-16 19:37:33 -0500

    Se agrega el package SortList el cual contiene el uso de sort e interfaz funcional ademas de orden natural de las listas

---
+++ JavaUtilPackage/out/production/JavaUtilPackage/WrappersClass/README.md
@@ -0,0 +1,9 @@
+## Las Clases Wrappers
+
+- Para cada primitivo hay una clase llamada Wrapper.
+- Para almacenar un primitivo en una colección, necesita crear un objeto que envuelva el valor.
+- La creación del objeto Wrapper se llama autoboxing.
+- La extracción del valor primitivo del objeto Wrapper se llama unboxing.
+- El autoboxing y unboxing ocurren automáticamente.
+- Las clases wrapper tienen varios métodos auxiliares, por ejemplo para el parsing.
+- Todas las clases wrappers que representan un valor numérico tienen la clase java.lang.Number como madre.
---
+++ JavaUtilPackage/src/SortList/ExecuteSort.java
@@ -0,0 +1,69 @@
+package SortList;
+
+import Resources.Cuenta;
+import Resources.CuentaAhorros;
+import Resources.CuentaCorriente;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ExecuteSort {
+
+  /**
+   * Contiene el metodo sort y una interfaz Funcional
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    List<Cuenta> cuentaList = new ArrayList<>();
+
+    CuentaAhorros cuentaAhorros = new CuentaAhorros();
+    cuentaAhorros.setNumber("234234");
+    cuentaList.add(cuentaAhorros);
+
+    CuentaAhorros cuentaAhorrosDos = new CuentaAhorros();
+    cuentaAhorrosDos.setNumber("457458");
+    cuentaList.add(cuentaAhorrosDos);
+
+    CuentaCorriente cuentaCorriente = new CuentaCorriente();
+    cuentaCorriente.setNumber("123123");
+    cuentaList.add(cuentaCorriente);
+
+    System.out.println("<<< Antes de ordenar >>>");
+
+    for (Cuenta cuenta : cuentaList) {
+      System.out.println(cuenta.getNumber());
+    }
+
+    //El metodo Sort recibe una interfaz funcional Comparator<T>
+    //Comparator<Cuenta> comparator = new OrdenadorPorNumeroCuenta();
+    cuentaList.sort(new OrdenadorPorNumeroCuenta());  //Se puede llamar la clase ya que la misma implementa Comparator
+
+    System.out.println("<<< Despues de ordenar >>>");
+
+    for (Cuenta cuenta : cuentaList) {
+      System.out.println(cuenta.getNumber());
+    }
+
+  }
+}
+
+/**
+ * Clase que compara y retorna los numeros de menor a mayor al metodo sort
+ */
+class OrdenadorPorNumeroCuenta implements Comparator<Cuenta> {
+
+  @Override
+  public int compare(Cuenta o1, Cuenta o2) {
+    Integer numberO1Cast = Integer.valueOf(o1.getNumber());
+    Integer numberO2Cast = Integer.valueOf(o2.getNumber());
+    if (numberO1Cast.equals(numberO2Cast)) {
+      return 0;
+    } else if (numberO1Cast > numberO2Cast) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+}
